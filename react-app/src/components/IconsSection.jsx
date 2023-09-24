3bc495ed284b7d8565c1e1210c8184f296f9bbe5
jtw007 <joshwu095@gmail.com>
2023-09-23 21:41:32 -0700

    icons section finished

--- react-app/src/components/IconsSection.jsx
+++ react-app/src/components/IconsSection.jsx
@@ -1,5 +1,13 @@
 import { iconsList } from '../Icons/iconsList'
 
+const iconsText = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.'
+}
+
+const iconAccordionText = {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+}
+
 const IconsSection = () => {
     const icons = iconsList.map((icon, idx) => {
             return (
@@ -23,6 +31,19 @@ const IconsSection = () => {
 
             <div id='icons-text-container'>
                 <p id='icons-text-title'>Lorem Ipsum Dolor Sit Amet</p>
+                <p id='icons-text'>{iconsText.text}</p>
+
+                <div id='icon-accordion'>
+                    <input 
+                        type='checkbox' 
+                        id='icons-trigger' 
+                    />
+                    <label 
+                        id='icon-accordion-label' 
+                        htmlFor='icons-trigger'>Lorem Ipsum
+                    </label>
+                    <p id='icon-text'>{iconAccordionText.text}</p>
+                </div>
             </div>
         </div>
     )
